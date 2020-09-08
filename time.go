0fe3b977cf5b8ff2f4ff205cf20c5ba4472c80fa
colynn.liu <colynn.liu@gmail.com>
2020-09-08 16:34:01 +0800

    modified func name

--- time.go
+++ time.go
@@ -98,11 +98,11 @@ func TimeFormatParse(formatStr string) string {
 	return res
 }
 
-// Comparison time
+// TimeComparison time
 // 0 equal,
 // 1 time1 greater than time2
 // 2 time1 less than time2
-func Comparison(time1, time2 time.Time) int {
+func TimeComparison(time1, time2 time.Time) int {
 	subSeconds := time1.Sub(time2).Seconds()
 	if subSeconds == 0 {
 		return 0
--- time_test.go
+++ time_test.go
@@ -58,20 +58,20 @@ func TestParseStrToDate(t *testing.T) {
 func TestComparison(t *testing.T) {
 	value1, _ := ParseStrToDate("2020-10-22", "YYYY-MM-DD", "Asia/Shanghai")
 	value2, _ := ParseStrToDate("2020-10-22", "YYYY-MM-DD", "Asia/Shanghai")
-	value := Comparison(value1, value2)
+	value := TimeComparison(value1, value2)
 	assert.Equal(t, 0, value)
 }
 
 func TestComparisonTimeZone(t *testing.T) {
 	value1, _ := ParseStrToDate("2020-10-22", "YYYY-MM-DD", "Asia/Shanghai")
 	value2, _ := ParseStrToDate("2020-10-22", "YYYY-MM-DD", "UTC")
-	value := Comparison(value1, value2)
+	value := TimeComparison(value1, value2)
 	assert.Equal(t, 2, value)
 }
 
 func TestComparisonTimeGreaterThan(t *testing.T) {
 	value1, _ := ParseStrToDate("2020-10-22", "YYYY-MM-DD", "Asia/Shanghai")
 	value2, _ := ParseStrToDate("2020-10-02", "YYYY-MM-DD", "Asia/Shanghai")
-	value := Comparison(value1, value2)
+	value := TimeComparison(value1, value2)
 	assert.Equal(t, 1, value)
 }
